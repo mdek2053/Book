60079e24d728c1c7b530f60abb60b39aa794861c
Milan de Koning <M.deKoning-1@student.tudelft.nl>
2021-11-27 17:31:56 +0100

    made it so that spring can run without a database(temporarily

--- services/authentication/src/main/java/nl/tudelft/sem11b/authentication/MainApplication.java
+++ services/authentication/src/main/java/nl/tudelft/sem11b/authentication/MainApplication.java
@@ -3,8 +3,10 @@ package nl.tudelft.sem11b.authentication;
 import nl.tudelft.sem11b.Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//This exclude line is here temporarily, so we dont have to set up a database yet
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MainApplication {
     public static void main(String[] args) {
         var service = new Service("authentication");
