fbaf0fc6167507f7df18b3182faa11ca4f0b8f09
Milan de Koning <M.deKoning-1@student.tudelft.nl>
2021-11-27 17:26:54 +0100

    added controller class

---
+++ services/authentication/src/main/java/nl/tudelft/sem11b/authentication/AuthenticationController.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem11b.authentication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/")
+    public String test(){
+        return "<h1> TEST </h1>";
+    }
+}
